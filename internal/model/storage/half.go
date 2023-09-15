6952a9a1addab4ca63adb17485d9a45e3ffadd5a
lwch <lwch748@gmail.com>
2023-09-15 17:48:25 +0800

    实现bfloat16的解析 #36

--- internal/model/storage/half.go
+++ internal/model/storage/half.go
@@ -43,13 +43,13 @@ func (*Half) Type() StorageType {
 
 // http://www.fox-toolkit.org/ftp/fasthalffloatconversion.pdf
 func u16toFloat(u16 uint16) float32 {
-	n := hfMantissaTable[hfOffsetTable[u16>>10]+uint32(u16&0x3ff)] + hfExponentTable[u16>>10]
+	n := mantissaTable[offsetTable[u16>>10]+uint32(u16&0x3ff)] + exponentTable[u16>>10]
 	return *(*float32)(unsafe.Pointer(&n))
 }
 
-var hfMantissaTable [2048]uint32
-var hfExponentTable [64]uint32
-var hfOffsetTable [64]uint32
+var mantissaTable [2048]uint32
+var exponentTable [64]uint32
+var offsetTable [64]uint32
 
 func init() {
 	initMantissaTable()
@@ -58,40 +58,40 @@ func init() {
 }
 
 func initMantissaTable() {
-	hfMantissaTable[0] = 0
+	mantissaTable[0] = 0
 	for i := 1; i <= 1023; i++ {
-		hfMantissaTable[i] = convertHFMantissa(i)
+		mantissaTable[i] = convertMantissa(i)
 	}
 	for i := 1024; i <= 2047; i++ {
-		hfMantissaTable[i] = 0x38000000 + uint32(i-1024)<<13
+		mantissaTable[i] = 0x38000000 + uint32(i-1024)<<13
 	}
 }
 
 func initExponentTable() {
-	hfExponentTable[0] = 0
-	hfExponentTable[31] = 0x47800000
-	hfExponentTable[32] = 0x80000000
-	hfExponentTable[63] = 0xC7800000
+	exponentTable[0] = 0
+	exponentTable[31] = 0x47800000
+	exponentTable[32] = 0x80000000
+	exponentTable[63] = 0xC7800000
 	for i := 1; i <= 30; i++ {
-		hfExponentTable[i] = uint32(i) << 23
+		exponentTable[i] = uint32(i) << 23
 	}
 	for i := 33; i <= 62; i++ {
-		hfExponentTable[i] = 0x80000000 + uint32(i-32)<<23
+		exponentTable[i] = 0x80000000 + uint32(i-32)<<23
 	}
 }
 
 func initOffsetTable() {
-	hfOffsetTable[0] = 0
-	hfOffsetTable[32] = 0
+	offsetTable[0] = 0
+	offsetTable[32] = 0
 	for i := 1; i <= 31; i++ {
-		hfOffsetTable[i] = 1024
+		offsetTable[i] = 1024
 	}
 	for i := 32; i <= 63; i++ {
-		hfOffsetTable[i] = 1024
+		offsetTable[i] = 1024
 	}
 }
 
-func convertHFMantissa(i int) uint32 {
+func convertMantissa(i int) uint32 {
 	m := uint32(i) << 13
 	e := uint32(0)
 	for m&0x00800000 == 0 {
--- internal/model/test/make.py
+++ internal/model/test/make.py
@@ -5,10 +5,10 @@ class Linear(nn.Module):
     def __init__(self):
         super().__init__()
         self.linear = nn.Parameter(
-            # torch.rand(2, 3, dtype=torch.float32),
-            torch.arange(6, dtype=torch.long).reshape(2, 3),
+            torch.rand(2, 3, dtype=torch.bfloat16),
+            # torch.arange(6, dtype=torch.long).reshape(2, 3),
             requires_grad=False)
-        # self.linear[0, 0] = -torch.nan
+        self.linear[0, 0] = torch.inf
         print(self.linear)
 
     def forward(self, x):
